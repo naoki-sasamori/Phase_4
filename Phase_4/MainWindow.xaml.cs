060d31495eade640d7e1e2c16bf00b67abe6da00
naoki-sasamori <naoki_sasamori@sobal.co.jp>
2020-08-18 13:24:22 +0900

    フェーズ4-6のレビュー指摘を反映

--- Phase_4/MainWindow.xaml.cs
+++ Phase_4/MainWindow.xaml.cs
@@ -26,50 +26,45 @@ namespace Phase_4
         }
 
         // アプリケーション処理開始時
-        private void Windows_ContentRendered(object sender, EventArgs e)
+        private void Windows_Loaded(object sender, EventArgs e)
         {
-            // テキストボックスに前回の値を設定
-            filePath.Text = Properties.Settings.Default.filePath;
-
             // ウィンドウに関する設定値を取得
-            LoadWiodowsInfo();
+            LoadWindowsInfo();
         }
 
         // アプリケーション終了時
         private void Windows_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            // テキストボックスの値を保存
-            Properties.Settings.Default.filePath = filePath.Text;
-            Properties.Settings.Default.Save();
-
             // ウィンドウに関する設定値を保持
             SaveWiodowsInfo();
         }
 
         // ウィンドウに関する設定値を取得
-        private void LoadWiodowsInfo()
+        private void LoadWindowsInfo()
         {
             var settings = Properties.Settings.Default;
 
+            this.filePath.Text = settings.filePath;
+
             if ( settings.WindowLeft >= 0 && (settings.WindowLeft + settings.WindowWidth) < SystemParameters.VirtualScreenWidth ) {
-                Left = settings.WindowLeft;
+                this.Left = settings.WindowLeft;
             }
 
             if ( settings.WindowTop >= 0 && (settings.WindowTop + settings.WindowHeight) < SystemParameters.VirtualScreenHeight ) {
-                Top = settings.WindowTop;
+                this.Top = settings.WindowTop;
             }
 
             if ( settings.WindowWidth > 0 && settings.WindowWidth <= SystemParameters.WorkArea.Width ) {
-                Width = settings.WindowWidth;
+                this.Width = settings.WindowWidth;
             }
 
             if ( settings.WindowHeight > 0 && settings.WindowHeight <= SystemParameters.WorkArea.Height ) {
-                Height = settings.WindowHeight;
+                this.Height = settings.WindowHeight;
             }
 
             if ( settings.WindowMaximized ) {
                 // ロード後に最大化
-                WindowState = WindowState.Maximized;
+                this.WindowState = WindowState.Maximized;
             }
         }
 
@@ -77,11 +72,13 @@ namespace Phase_4
         private void SaveWiodowsInfo()
         {
             var settings = Properties.Settings.Default;
-            settings.WindowMaximized    = WindowState == WindowState.Maximized;
-            settings.WindowLeft         = Left;
-            settings.WindowTop          = Top;
-            settings.WindowWidth        = Width;
-            settings.WindowHeight       = Height;
+
+            settings.filePath           = this.filePath.Text;
+            settings.WindowMaximized    = this.WindowState == WindowState.Maximized;
+            settings.WindowLeft         = this.Left;
+            settings.WindowTop          = this.Top;
+            settings.WindowWidth        = this.Width;
+            settings.WindowHeight       = this.Height;
             settings.Save();
         }
     }
